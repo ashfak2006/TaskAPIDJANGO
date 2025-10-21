437fa4f2cf4d3750d3a03dc6ecade5e5394925b2
ashfak ameer <ameerashfak90@gmail.com>
2025-10-21 20:37:50 +0530

    docer image build completed ready to push

--- taskapi/settings.py
+++ taskapi/settings.py
@@ -26,7 +26,8 @@ SECRET_KEY = 'django-insecure-5-4wav&!i7852pudsjo-vz58$i^$t27amx6h3#$ak3(u4(w4#i
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
+
 
 
 # Application definition
